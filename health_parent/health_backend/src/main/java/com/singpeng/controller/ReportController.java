6034d57b2427ad73e27a779fadae46525bbb74a8
singpeng <syingpeng6181@outlook.com>
2023-05-10 18:28:39 +0800

    第八次提交, 完成报表输出功能和了解缓存穿透的知识。

--- health_parent/health_backend/src/main/java/com/singpeng/controller/ReportController.java
+++ health_parent/health_backend/src/main/java/com/singpeng/controller/ReportController.java
@@ -1,14 +1,32 @@
 package com.singpeng.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.dubbo.remoting.transport.ExceedPayloadLimitException;
 import com.singpeng.constant.MessageConstant;
 import com.singpeng.entity.Result;
 import com.singpeng.service.MemberService;
+import com.singpeng.service.ReportService;
 import com.singpeng.service.SetmealService;
 import com.sun.tools.classfile.ConstantPool;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectStreamClass;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
@@ -21,6 +39,8 @@ public class ReportController {
     @Reference
     private SetmealService setmealService;
 
+    @Reference
+    private ReportService reportService;
     /**
      * 会员数量统计
      *
@@ -59,4 +79,163 @@ public class ReportController {
         map.put("setmealName",setmealName);
         return new Result(true,MessageConstant.GET_SETMEAL_COUNT_REPORT_SUCCESS,map);
     }
+
+    /**
+     * 统计运营数据
+     */
+    @RequestMapping("/getBusinessReportData")
+    public Result getBusinessReportData(){
+        try {
+            Map<String, Object> result = reportService.getBusinessReport();
+            return new Result(true,MessageConstant.GET_BUSINESS_REPORT_SUCCESS,result);
+        } catch (Exception e){
+            e.printStackTrace();
+            return new Result(false,MessageConstant.GET_BUSINESS_REPORT_FAIL);
+        }
+    }
+
+    /**
+     * 导出excel表
+     */
+    @RequestMapping("/exportBusinessReport")
+    public Result exportBusinessReport (HttpServletRequest request, HttpServletResponse response){
+        try {
+            //远程调用报表服务获得的数据
+            Map<String,Object> result = reportService.getBusinessReport();
+            //从结果中获得数据并写入excel表
+            String reportDate = (String) result.get("reportDate");
+            Integer todayNewMember = (Integer) result.get("todayNewMember");
+            Integer totalMember = (Integer) result.get("totalMember");
+            Integer thisWeekNewMember = (Integer) result.get("thisWeekNewMember");
+            Integer thisMonthNewMember = (Integer) result.get("thisMonthNewMember");
+            Integer todayOrderNumber = (Integer) result.get("todayOrderNumber");
+            Integer thisWeekOrderNumber = (Integer) result.get("thisWeekOrderNumber");
+            Integer thisMonthOrderNumber = (Integer) result.get("thisMonthOrderNumber");
+            Integer todayVisitsNumber = (Integer) result.get("todayVisitsNumber");
+            Integer thisWeekVisitsNumber = (Integer) result.get("thisWeekVisitsNumber");
+            Integer thisMonthVisitsNumber = (Integer) result.get("thisMonthVisitsNumber");
+            List<Map> hotSetmeal = (List<Map>) result.get("hotSetmeal");
+            //获取excel的绝对路径
+            String templateRealPath = request.getSession().getServletContext().getRealPath("template") + File.separator + "report_template.xlsx";
+            //读取excel
+            XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream(new File(templateRealPath)));
+            XSSFSheet sheet = workbook.getSheetAt(0);
+
+            XSSFRow row = sheet.getRow(2);
+            row.getCell(5).setCellValue(reportDate);//日期
+
+            row = sheet.getRow(4);
+            row.getCell(5).setCellValue(todayNewMember);//新增会员数（本日）
+            row.getCell(7).setCellValue(totalMember);//总会员数
+
+            row = sheet.getRow(5);
+            row.getCell(5).setCellValue(thisWeekNewMember);//本周新增会员数
+            row.getCell(7).setCellValue(thisMonthNewMember);//本月新增会员数
+
+            row = sheet.getRow(7);
+            row.getCell(5).setCellValue(todayOrderNumber);//今日预约数
+            row.getCell(7).setCellValue(todayVisitsNumber);//今日到诊数
+
+            row = sheet.getRow(8);
+            row.getCell(5).setCellValue(thisWeekOrderNumber);//本周预约数
+            row.getCell(7).setCellValue(thisWeekVisitsNumber);//本周到诊数
+
+            row = sheet.getRow(9);
+            row.getCell(5).setCellValue(thisMonthOrderNumber);//本月预约数
+            row.getCell(7).setCellValue(thisMonthVisitsNumber);//本月到诊数
+
+            int rowNum = 12;
+            for (Map map : hotSetmeal) {
+                String name = (String) map.get("name");
+                Long setmealCount = (Long) map.get("setmeal_count");
+                BigDecimal proportion = (BigDecimal) map.get("proportion");
+                row = sheet.getRow(rowNum++);
+                row.getCell(4).setCellValue(name);//套餐名称
+                row.getCell(5).setCellValue(setmealCount);//预约数量
+                row.getCell(6).setCellValue(proportion.doubleValue());//占比
+                //row.getCell(6).setCellValue(NumberFormat.getPercentInstance().format(proportion.doubleValue()));//占比
+            }
+            //通过输出流对文件进行下载
+            ServletOutputStream out = response.getOutputStream();
+            response.setContentType("application/vnd.ms-excel");
+            response.setHeader("content-Disposition","attachment;filename=report.xlsx");
+            workbook.write(out);
+            out.flush();
+            out.close();
+            workbook.close();
+            return null;
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Result(false,MessageConstant.GET_BUSINESS_REPORT_FAIL);
+        }
+    }
+
+    //导出PDF
+    @RequestMapping("/exportBusinessReport4PDF")
+    public Result exportBusinessReport4PDF(HttpServletRequest request, HttpServletResponse response){
+        try {
+            Map<String,Object> result = reportService.getBusinessReport();
+            List<Map> hotSetmeal = (List<Map>) result.get("hotSetmeal");
+            //动态获取模板文件绝对磁盘路径
+            //模板路径
+            String jrxmlPath =
+                    request.getSession().getServletContext().getRealPath("template") + File.separator + "health_business3.jrxml";
+            //编译完的2进制文件路径
+            String jasperPath =
+                    request.getSession().getServletContext().getRealPath("template") + File.separator + "health_business3.jasper";
+            //编译模板
+            JasperCompileManager.compileReportToFile(jrxmlPath,jasperPath);
+            //填充数据--javaBean
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperPath,result,new JRBeanCollectionDataSource(hotSetmeal));
+            //创建输出流
+            ServletOutputStream out = response.getOutputStream();
+            response.setContentType("application/pdf");
+            response.setHeader("content-Disposition", "attachment;filename=report.pdf");
+            //输出文件
+            JasperExportManager.exportReportToPdfStream(jasperPrint,out);
+            out.flush();
+            out.close();
+            return null;
+
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Result(false,MessageConstant.GET_BUSINESS_REPORT_FAIL);
+        }
+    }
+
+
+    /*@RequestMapping("/exportBusinessReport4PDF")
+    public Result exportBusinessReport4PDF(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            Map<String, Object> result = reportService.getBusinessReport();
+
+            //取出返回结果数据，准备将报表数据写入到PDF文件中
+            List<Map> hotSetmeal = (List<Map>) result.get("hotSetmeal");
+
+            //动态获取模板文件绝对磁盘路径
+            String jrxmlPath =
+                    request.getSession().getServletContext().getRealPath("template") + File.separator + "health_business3.jrxml";
+            String jasperPath =
+                    request.getSession().getServletContext().getRealPath("template") + File.separator + "health_business3.jasper";
+            //编译模板
+            JasperCompileManager.compileReportToFile(jrxmlPath, jasperPath);
+
+            //填充数据---使用JavaBean数据源方式填充
+            JasperPrint jasperPrint =
+                    JasperFillManager.fillReport(jasperPath,result,
+                            new JRBeanCollectionDataSource(hotSetmeal));
+
+            ServletOutputStream out = response.getOutputStream();
+            response.setContentType("application/pdf");
+            response.setHeader("content-Disposition", "attachment;filename=report.pdf");
+
+            //输出文件
+            JasperExportManager.exportReportToPdfStream(jasperPrint,out);
+
+            return null;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, MessageConstant.GET_BUSINESS_REPORT_FAIL);
+        }
+    }*/
 }
---
+++ health_parent/health_interface/src/main/java/com/singpeng/service/ReportService.java
@@ -0,0 +1,7 @@
+package com.singpeng.service;
+
+import java.util.Map;
+
+public interface ReportService {
+    Map<String,Object> getBusinessReport() throws Exception;
+}
